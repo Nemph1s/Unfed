c652b41d8af2c404f7beb91ba1b6156912d5b6c3
Vladimir.Martyniuk <inemphis@gmail.com>
2017-02-01 18:32:34 +0200

    [utils] add macro for singleton; [global] add resources manager; [global] vcxproj filters updated

---
+++ Classes/Utils/PlatformMacros.h
@@ -0,0 +1,36 @@
+/**
+* @file Utils/PlatformMacros.hpp
+* Copyright (C) 2017
+* Company       Octohead LTD
+*               All Rights Reserved
+*               Secrecy Level STRICTLY CONFIDENTIAL
+*
+* @author VMartyniuk
+*/
+
+#pragma once
+
+/** @def CREATE_SINGLETON(__TYPE__)
+* Define a singleton functions for a specific type
+*
+* @param __TYPE__  class type
+*/
+#define CREATE_SINGLETON(__TYPE__) \
+    public: \
+    static __TYPE__& Instance() \
+    { \
+        static __TYPE__ myInstance; \
+        return myInstance; \
+    } \
+    static __TYPE__* getInstance() \
+    { \
+        return &__TYPE__::Instance(); \
+    } \
+    protected: \
+    __TYPE__ () {}; \
+    __TYPE__ (__TYPE__ const&) = delete; \
+    __TYPE__ ( __TYPE__ &&) = delete; \
+    __TYPE__ & operator=( __TYPE__ const&) = delete; \
+    __TYPE__ & operator=( __TYPE__ &&) = delete; \
+    ~ __TYPE__ () {}; \
+    
