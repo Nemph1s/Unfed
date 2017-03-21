12d4988bc942fc01c2f58f843dd59d3e714f1dd1
Nemph1s <nemphis@ukr.net>
2017-03-21 23:40:30 +0200

    add draft version of jump animation #94

--- Classes/Managers/AnimationsManager.cpp
+++ Classes/Managers/AnimationsManager.cpp
@@ -436,6 +436,50 @@ void _AnimationsManager::animateBouncingObj(BaseObj * obj)
 }
 
 //--------------------------------------------------------------------
+void _AnimationsManager::animateJumpingObj(BaseObj * obj)
+//--------------------------------------------------------------------
+{
+    CC_ASSERT(obj);
+
+    float duration = 0.2f;
+    auto moveAction = MoveBy::create(duration, Vec2(0.0f, -10.0f));
+    auto easeMoveOut = EaseOut::create(moveAction, duration);
+    auto scaleXAction = ScaleTo::create(duration, 1.2f, 0.8f);
+    auto easeScaleXOut = EaseOut::create(scaleXAction, duration);
+
+    auto reverseScaleXCallback = CallFunc::create([obj]() {
+        float duration = 0.2f;
+        auto reverseScaleXAction = ScaleTo::create(duration, 1.0f, 1.0f);
+        auto reverseEaseScaleXOut = EaseOut::create(reverseScaleXAction, duration);
+        auto reverseMoveAction = MoveBy::create(duration, Vec2(0.0f, 10.0f));
+        auto reverseEaseMoveOut = EaseOut::create(reverseMoveAction, duration);
+        if (obj) {
+            auto sprite = obj->getSpriteNode();
+            if (sprite) {
+                sprite->runAction(reverseEaseScaleXOut);
+                sprite->runAction(reverseEaseMoveOut);
+            }
+        }
+    });
+
+    duration = 0.2f;
+    auto scaleYAction = ScaleTo::create(duration, 0.8f, 1.2f);
+    auto easeScaleYOut = EaseOut::create(scaleYAction, duration);
+
+    auto reverseScaleYAction = ScaleTo::create(duration * 2, 1.0f, 1.0f);
+    auto reverseEaseScaleYOut = EaseOut::create(reverseScaleYAction, duration);
+
+    auto speed = 2.0f;
+    auto sprite = obj->getSpriteNode();
+    sprite->runAction(Speed::create(easeMoveOut, speed));
+    sprite->runAction(Speed::create(easeScaleXOut, speed));
+    auto seq1 = Sequence::create(DelayTime::create(0.2f), reverseScaleXCallback, nullptr);
+    sprite->runAction(Speed::create(seq1, speed));
+    auto seq2 = Sequence::create(DelayTime::create(0.4f), easeScaleYOut, reverseEaseScaleYOut, nullptr);
+    sprite->runAction(Speed::create(seq2, speed));
+}
+
+//--------------------------------------------------------------------
 void _AnimationsManager::animateHintSwap(CommonTypes::Set* objects, cocos2d::CallFunc* completion)
 //--------------------------------------------------------------------
 {
@@ -444,7 +488,7 @@ void _AnimationsManager::animateHintSwap(CommonTypes::Set* objects, cocos2d::Cal
     for (auto itObj = objects->begin(); itObj != objects->end(); itObj++) {
         auto obj = dynamic_cast<BaseObj*>(*itObj);
         if (obj) {
-            animateBouncingObj(obj);
+            animateJumpingObj(obj);
         }
     }
 
